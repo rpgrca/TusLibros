8cce4d41279be6b415b0b1d7f5bf7fadfb2910e9
Roberto <rpgrca@gmail.com>
2020-04-24 00:33:23 -0300

    fin iteracion 1

--- TusLibros.UnitTests/CartShould.cs
+++ TusLibros.UnitTests/CartShould.cs
@@ -100,6 +100,101 @@ namespace TusLibros.UnitTests
         {
             Assert.Throws<ArgumentException>(() => new Cart(null));
         }
+
+        [Fact]
+        public void GivenANewCartAndACopyOfItems_WhenAddingAnItemToCart_ThenTheCopyOfItemsDidNotAddTheItem()
+        {
+            var cart = GetCartWithACatalogWithValidItem();
+            var items = cart.GetBooks();
+            cart.Add(VALID_ITEM, 1);
+
+            Assert.NotEqual(items.Count, cart.Count);
+        }
+
+        [Fact]
+        public void GivenANewCartWithAnItem_WhenChangingTheItemInTheCopyOfItems_ThenTheCartDidNotChange()
+        {
+            var cart = GetCartWithACatalogWithValidItem();
+            cart.Add(VALID_ITEM, 1);
+
+            var items = cart.GetBooks();
+            items[0] = INVALID_ITEM;
+            Assert.Single(cart.GetBooks(), VALID_ITEM);
+        }
+
+        [Fact]
+        public void WithANewCart_WhenCountingInexistantItems_ThenReturnsZero()
+        {
+            var cart = GetCartWithEmptyCatalog();
+            var count = cart.GetCount(VALID_ITEM);
+
+            Assert.Equal(0, count);
+        }
+
+        [Fact]
+        public void WithANewCartWithOneItem_WhenCountingThatItem_ThenReturnsOne()
+        {
+            var cart = GetCartWithACatalogWithValidItem();
+            cart.Add(VALID_ITEM, 2);
+            var count = cart.GetCount(VALID_ITEM);
+
+            Assert.Equal(2, count);
+        }
+
+        [Fact]
+        public void WithANewCartWithOneItem_WhenCountingAnotherItem_ThenReturnsZero()
+        {
+            var cart = GetCartWithACatalogWithValidItem();
+            cart.Add(VALID_ITEM, 1);
+            var count = cart.GetCount(ANOTHER_VALID_ITEM);
+
+            Assert.Equal(0, count);
+        }
+
+        [Fact]
+        public void WithANewCart_WhenAddingTheSameItemTwice_ThenReturnsSum()
+        {
+            var cart = GetCartWithACatalogWithValidItem();
+            cart.Add(VALID_ITEM, 4);
+            cart.Add(VALID_ITEM, 6);
+            var count = cart.GetCount(VALID_ITEM);
+
+            Assert.Equal(10, count);
+        }
+
+        [Fact]
+        public void WithANewCart_WhenAddingDifferentItemsTwice_ThenReturnsDifferentCounts()
+        {
+            var cart = GetCartWithACatalogWithTwoValidItems();
+            cart.Add(VALID_ITEM, 4);
+            cart.Add(ANOTHER_VALID_ITEM, 6);
+
+            Assert.Equal(4, cart.GetCount(VALID_ITEM));
+            Assert.Equal(6, cart.GetCount(ANOTHER_VALID_ITEM));
+        }
+
+        [Fact]
+        public void WithANewCart_WhenCheckingForInexistantItems_ThenReturnsFalse()
+        {
+            var cart = GetCartWithEmptyCatalog();
+            Assert.False(cart.Contains(VALID_ITEM));
+        }
+
+        [Fact]
+        public void WithANewCartWithOneItem_WhenCheckingForThatItem_ThenReturnsTrue()
+        {
+            var cart = GetCartWithACatalogWithValidItem();
+            cart.Add(VALID_ITEM, 1);
+            Assert.True(cart.Contains(VALID_ITEM));
+        }
+
+        [Fact]
+        public void WithANewCartWithOneItem_WhenCheckingAnotherItem_ThenReturnsFalse()
+        {
+            var cart = GetCartWithACatalogWithValidItem();
+            cart.Add(VALID_ITEM, 1);
+            Assert.False(cart.Contains(ANOTHER_VALID_ITEM));
+        }
     }
 }
 
--- TusLibros/Cart.cs
+++ TusLibros/Cart.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Collections.Generic;
 using System;
 
@@ -10,7 +11,12 @@ namespace TusLibros.UnitTests
         private readonly List<object> _items;
         private readonly List<object> _catalog;
 
-        public List<object> GetBooks() => _items;
+        public List<object> GetBooks()
+        {
+            var itemsToReturn = new List<object>();
+            _items.ForEach(p => itemsToReturn.Add(p));
+            return itemsToReturn;
+        }
 
         public int Count => _items.Count;
 
@@ -39,5 +45,9 @@ namespace TusLibros.UnitTests
                 _items.Add(item);
             }
         }
+
+        public int GetCount(object item) => _items.Count(p => p == item);
+
+        public bool Contains(object item) => _items.Contains(item);
     }
 }
