e879434c102b8ea77cc4f877cc8b24405076df7d
Roberto <rpgrca@gmail.com>
2020-04-23 23:42:43 -0300

    update 1

--- TusLibros.UnitTests/CartShould.cs
+++ TusLibros.UnitTests/CartShould.cs
@@ -1,39 +1,73 @@
 using System.Runtime.InteropServices;
 using System;
 using Xunit;
+using System.Collections.Generic;
 
 namespace TusLibros.UnitTests
 {
     public class CartShould
     {
+        private readonly object VALID_ITEM = new object();
+        private readonly object ANOTHER_VALID_ITEM = new object();
+        private readonly object INVALID_ITEM = new object();
+
+        private List<object> GetCatalogWithValidItem() => new List<object>() { VALID_ITEM };
+        private List<object> GetCatalogWithTwoValidItems() => new List<object>() { VALID_ITEM, ANOTHER_VALID_ITEM };
+        private Cart GetCartWithACatalogWithValidItem() => new Cart(GetCatalogWithValidItem());
+        private Cart GetCartWithACatalogWithTwoValidItems() => new Cart(GetCatalogWithTwoValidItems());
+        private Cart GetCartWithEmptyCatalog() => new Cart(new List<object>());
+
+        [Fact]
+        public void GivenACartWithACatalogWithOneItem_WhenAnItemThatIsNotInCatalogIsAdded_ThenAnExceptionIsThrown()
+        {
+            var cart = GetCartWithACatalogWithValidItem();
+            var exception = Assert.Throws<Exception>(() => cart.Add(INVALID_ITEM, 1));
+            Assert.Contains("No existe", exception.Message);
+        }
+
+        [Fact]
+        public void GivenACartWithACatalogWithOneItem_WhenAnItemThatIsNotInCatalogIsAdded_ThenTheCartStaysEmpty()
+        {
+            var cart = GetCartWithACatalogWithValidItem();
+
+            try
+            {
+                cart.Add(INVALID_ITEM, 1);
+            }
+            catch (Exception)
+            {
+                Assert.True(cart.IsEmpty());
+            }
+        }
+
         [Fact]
         public void WhenCreatingANewCart_ThenTheCartHasZeroItems()
         {
-            var cart = new Cart();
+            var cart = GetCartWithEmptyCatalog();
             Assert.Equal(0, cart.Count);
         }
 
         [Fact]
         public void GivenANewCart_WhenAddingAnItem_ThenTheCartHasOneItem()
         {
-            var cart = new Cart();
-            cart.Add(new object(), 1);
+            var cart = GetCartWithACatalogWithValidItem();
+            cart.Add(VALID_ITEM, 1);
             Assert.Equal(1, cart.Count);
         }
 
         [Fact]
         public void GivenANewCart_WhenAddingTwoItems_ThenTheCartHasTwoItems()
         {
-            var cart = new Cart();
-            cart.Add(new object(), 1);
-            cart.Add(new object(), 1);
+            var cart = GetCartWithACatalogWithTwoValidItems();
+            cart.Add(VALID_ITEM, 1);
+            cart.Add(ANOTHER_VALID_ITEM, 1);
             Assert.Equal(2, cart.Count);
         }
 
         [Fact]
         public void WhenCreatingANewCart_ThenTheCartHasNoBooks()
         {
-            var cart = new Cart();
+            var cart = GetCartWithEmptyCatalog();
             Assert.Empty(cart.GetBooks());
         }
 
@@ -42,22 +76,22 @@ namespace TusLibros.UnitTests
         [InlineData(-1)]
         public void GivenANewCart_WhenTryingToAddInvalidCountOfItems_ThenAnExceptionIsThrown(int count)
         {
-            var cart = new Cart();
-            Assert.Throws<ArgumentException>(() => cart.Add(new object(), count));
+            var cart = GetCartWithACatalogWithValidItem();
+            Assert.Throws<ArgumentException>(() => cart.Add(VALID_ITEM, count));
         }
 
         [Fact]
         public void GivenANewCart_WhenAskedIsEmpty_ThenItShouldReturnTrue()
         {
-            var cart = new Cart();
+            var cart = GetCartWithEmptyCatalog();
             Assert.True(cart.IsEmpty());
         }
 
         [Fact]
         public void GivenACartWithOneItem_WhenAskedIsEmpty_ThenItShouldReturnFalse()
         {
-            var cart = new Cart();
-            cart.Add(new object(), 1);
+            var cart = GetCartWithACatalogWithValidItem();
+            cart.Add(VALID_ITEM, 1);
             Assert.False(cart.IsEmpty());
         }
     }
--- TusLibros/Cart.cs
+++ TusLibros/Cart.cs
@@ -1,29 +1,42 @@
 using System.Collections.Generic;
-using System.Collections;
 using System;
 
 namespace TusLibros.UnitTests
 {
     public class Cart
     {
-        private readonly List<object> _items = new List<object>();
+        private const string NOT_IN_CATALOG_ERROR = "No existe en el catálogo";
+
+        private readonly List<object> _items;
+        private readonly List<object> _catalog;
 
         public List<object> GetBooks() => _items;
 
         public int Count => _items.Count;
 
-        public bool IsEmpty() => Count == 0;
+        public bool IsEmpty() => _items.Count == 0;
 
-        public void Add(object isbn, int count)
+        public Cart(List<object> catalog)
+        {
+            _catalog = catalog ?? throw new ArgumentException();
+            _items = new List<object>();
+        }
+
+        public void Add(object item, int count)
         {
             if (count < 1)
             {
                 throw new ArgumentException();
             }
 
+            if (! _catalog.Contains(item))
+            {
+                throw new Exception(NOT_IN_CATALOG_ERROR);
+            }
+
             for (int i = 0; i < count; i++)
             {
-                _items.Add(isbn);
+                _items.Add(item);
             }
         }
     }
