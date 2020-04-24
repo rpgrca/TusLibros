93f22ddbd9b0580b715a4c1d61e234bbf62cc228
Roberto <rpgrca@gmail.com>
2020-04-24 09:23:14 -0300

    implemented checkout

--- TusLibros/Cashier.cs
+++ TusLibros/Cashier.cs
@@ -1,9 +1,17 @@
+using System;
+using TusLibros.UnitTests;
+
 namespace TusLibros
 {
     public class Cashier
     {
         public Cashier()
         {
+        }
+
+        public void Checkout(Cart cart)
+        {
+            _ = cart ?? throw new ArgumentException();
         }
     }
 }
