914468b6b6b7861c8cec5f9b00e00598e55d677d
Roberto <rpgrca@gmail.com>
2020-04-26 15:26:22 -0300

    fixed namespaces

--- TusLibros/Cart.cs
+++ TusLibros/Cart.cs
@@ -2,7 +2,7 @@ using System.Linq;
 using System.Collections.Generic;
 using System;
 
-namespace TusLibros.UnitTests
+namespace TusLibros
 {
     public class Cart
     {
--- TusLibros/Cashier.cs
+++ TusLibros/Cashier.cs
@@ -1,7 +1,6 @@
 using System.Linq;
 using System.Collections.Generic;
 using System;
-using TusLibros.UnitTests;
 
 namespace TusLibros
 {
