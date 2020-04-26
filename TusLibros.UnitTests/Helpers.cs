dbc7f1b402f4a8484b4b83a1f753bac961fff435
Roberto <rpgrca@gmail.com>
2020-04-26 15:26:28 -0300

    formatting

--- TusLibros.UnitTests/Helpers.cs
+++ TusLibros.UnitTests/Helpers.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using TusLibros.UnitTests.MerchantFakes;
 
@@ -16,38 +17,30 @@ namespace TusLibros.UnitTests
         public const string SUCCESSFUL_TRANSACTION_ID = "Todo bien";
 
         public static List<object> GetCatalogWithValidItem() => new List<object>() { VALID_ITEM };
-        public static System.Collections.Generic.List<object> GetCatalogWithTwoValidItems() => new List<object>() { VALID_ITEM, ANOTHER_VALID_ITEM };
+        public static List<object> GetCatalogWithTwoValidItems() => new List<object>() { VALID_ITEM, ANOTHER_VALID_ITEM };
         public static Cart GetCartWithACatalogWithValidItem() => new Cart(GetCatalogWithValidItem());
         public static Cart GetCartWithACatalogWithTwoValidItems() => new Cart(GetCatalogWithTwoValidItems());
         public static Cart GetCartWithEmptyCatalog() => new Cart(new List<object>());
+
         public static Cart GetCartWithOneItem()
         {
             var cart = GetCartWithACatalogWithValidItem();
             cart.Add(VALID_ITEM, 1);
             return cart;
         }
-        public static Cashier GetCashierWithPricelistWithOneItemAndDummyMerchant()
-        {
-            return new Cashier(GetPricelistWithOneValidItem(VALID_PRICE), new DummyMerchant());
-        }
-        public static Dictionary<object, decimal> GetPricelistWithOneValidItem(decimal price)
-        {
-            return new Dictionary<object, decimal>
-            {
-                { VALID_ITEM, price }
-            };
-        }
-        public static Dictionary<object, decimal> GetPricelistWithTwoValidItems()
-        {
-            return new Dictionary<object, decimal>
-            {
+
+        public static Cashier GetCashierWithPricelistWithOneItemAndDummyMerchant() =>
+            new Cashier(GetPricelistWithOneValidItem(VALID_PRICE), new DummyMerchant());
+
+        public static Dictionary<object, decimal> GetPricelistWithOneValidItem(decimal price) =>
+            new Dictionary<object, decimal> { { VALID_ITEM, price } };
+
+        public static Dictionary<object, decimal> GetPricelistWithTwoValidItems() =>
+            new Dictionary<object, decimal> {
                 { VALID_ITEM, VALID_PRICE },
-                { ANOTHER_VALID_ITEM, ANOTHER_VALID_PRICE }
-            };
-        }
-        public static Dictionary<object, decimal> GetEmptyPricelist()
-        {
-            return new Dictionary<object, decimal>();
-        }
+                { ANOTHER_VALID_ITEM, ANOTHER_VALID_PRICE } };
+
+        public static Dictionary<object, decimal> GetEmptyPricelist() =>
+            new Dictionary<object, decimal>();
     }
 }
