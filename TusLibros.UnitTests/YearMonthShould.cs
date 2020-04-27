a8899e8294618bc3066961606f7ece7759d76fbb
Roberto <rpgrca@gmail.com>
2020-04-26 23:34:53 -0300

    added missing tests

--- TusLibros.UnitTests/YearMonthShould.cs
+++ TusLibros.UnitTests/YearMonthShould.cs
@@ -10,20 +10,33 @@ namespace TusLibros.UnitTests
         [InlineData(INVALID_YEAR, VALID_MONTH)]
         [InlineData(VALID_YEAR, INVALID_MONTH)]
         [InlineData(INVALID_YEAR, INVALID_MONTH)]
-        public void GivenAnInvalidExpirationDate_WhenCreatingAYearMonth_ThenThrowsAnException(int year, int month)
+        public void GivenAnInvalidDate_WhenCreatingAYearMonth_ThenThrowsAnException(int year, int month)
         {
             var exception = Assert.Throws<ArgumentException>(() => new YearMonth(year, month));
             Assert.Equal(YearMonth.DATE_IS_INVALID_ERROR, exception.Message);
         }
 
-        [Theory]
-        [InlineData(INVALID_YEAR, VALID_MONTH)]
-        [InlineData(VALID_YEAR, INVALID_MONTH)]
-        [InlineData(INVALID_YEAR, INVALID_MONTH)]
-        public void GivenAnInvalidCurrentDate_WhenCreatingAYearMonth_ThenThrowsAnException(int year, int month)
+        [Fact]
+        public void GivenTwoDifferentYearMonths_WhenComparingOlder_ThenItChecksCorrectly()
         {
-            var exception = Assert.Throws<ArgumentException>(() => new YearMonth(year, month));
-            Assert.Equal(YearMonth.DATE_IS_INVALID_ERROR, exception.Message);
+            var olderYearMonth = new YearMonth(1999, 12);
+            var newerYearMonth = new YearMonth(2020, 4);
+            var comparison = olderYearMonth < newerYearMonth;
+            Assert.True(comparison);
         }
+
+        [Fact]
+        public void GivenTwoDifferentYearMonths_WhenComparingNewer_ThenItChecksCorrectly()
+        {
+            var olderYearMonth = new YearMonth(1999, 12);
+            var newerYearMonth = new YearMonth(2020, 4);
+            var comparison = olderYearMonth > newerYearMonth;
+            Assert.False(comparison);
+        }
+
+        // TODO: GivenTwoDifferentYearMonths_WhenComparingEqual_ThenReturnsFalse
+        // TODO: GivenTwoEqualYearMonths_WhenComparingOlder_ThenReturnsFalse
+        // TODO: GivenTwoEqualYearMonths_WhenComparingNewer_ThenReturnsFalse
+        // TODO: GivenTwoEqualYearMonths_WhenComparingEqual_ThenReturnsTrue
     }
 }
