02e3cb8723c61a941f59e1a913a57028bd7681af
Roberto <rpgrca@gmail.com>
2020-04-28 15:07:30 -0300

    refactoring

--- TusLibros.API.UnitTests/ClockShould.cs
+++ TusLibros.API.UnitTests/ClockShould.cs
@@ -9,20 +9,20 @@ namespace TusLibros.API.UnitTests
         public void WithNewClock_WhenAskedIfExpiresOnExpiredDate_ThenReturnsFalse()
         {
             var clock = new Clock();
-            var lowerDateTime = new DateTime(2020, 4, 28, 7, 0, 0);
-            var higherDateTime = new DateTime(2020, 4, 28, 7, 0, 1);
+            var expirationDateTime = new DateTime(2020, 4, 28, 7, 0, 0);
+            var currentDateTime = new DateTime(2020, 4, 28, 7, 0, 1);
 
-            Assert.False(clock.Has(lowerDateTime).ExpiredOn(higherDateTime));
+            Assert.True(clock.Has(expirationDateTime).ExpiredOn(currentDateTime));
         }
 
         [Fact]
         public void WithNewClock_WhenAskedIfExpiresOnExpiredDate_ThenReturnsTrue()
         {
             var clock = new Clock();
-            var lowerDateTime = new DateTime(2020, 4, 28, 7, 0, 0);
-            var higherDateTime = new DateTime(2020, 4, 28, 7, 0, 1);
+            var currentDateTime = new DateTime(2020, 4, 28, 7, 0, 0);
+            var expirationDateTime = new DateTime(2020, 4, 28, 7, 0, 1);
 
-            Assert.True(clock.Has(higherDateTime).ExpiredOn(lowerDateTime));
+            Assert.False(clock.Has(expirationDateTime).ExpiredOn(currentDateTime));
         }
     }
 }
