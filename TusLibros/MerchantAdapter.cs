c83e14967b57a045c1ef88f044358020e4900dd5
Roberto <rpgrca@gmail.com>
2020-04-27 21:00:51 -0300

    renamed Merchant to MerchantAdapter

--- TusLibros/MerchantAdapter.cs
+++ TusLibros/MerchantAdapter.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace TusLibros
 {
-    public class Merchant : IMerchantAdapter
+    public class MerchantAdapter : IMerchantAdapter
     {
         public const string CARD_IS_STOLEN_ERROR = "La tarjeta de crédito fue robada.";
         public const string ACCOUNT_HAS_NO_MONEY_ERROR = "La cuenta no tiene fondos.";
